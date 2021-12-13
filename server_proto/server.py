c123c6ef55aa31bc2e4c8d2b4798d526f175235d
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-12-13 05:44:06 +0530

    Room alloc endpoint init

--- server_proto/server.py
+++ server_proto/server.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, jsonify
 from flask_bcrypt import Bcrypt
 import pymongo
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-import os
+import uuid
 
 app = Flask(__name__)
 bcrypt=Bcrypt(app)
@@ -13,9 +13,10 @@ bcrypt=Bcrypt(app)
 app.config['SECRET_KEY']='chickenmutton'
 jwt=JWTManager(app)
 
-client = pymongo.MongoClient('locahost', 27017)
+client = pymongo.MongoClient('localhost', 27017)
 db = client.classroom_allocation
 collection = db.users
+col_room_allocs = db.room_allocs
 
 # collection.create_index([('user_id', 'text')], unique=True)
 
@@ -67,3 +68,48 @@ def protected():
 	# Access the identity of the current user with get_jwt_identity
 	current_user = get_jwt_identity()
 	return {'logged_in_as': current_user}, 200
+
+@app.route("/user/check_alloc", methods=["POST"])
+def user_check_alloc():
+	user_params = request.json
+
+	# try:
+	# 	user_params["date"]
+	# except:
+	# 	return jsonify({"message": "Missing params, try again!"}), 400
+	return jsonify({"message": "Test works, ez :)"})
+
+@app.route("/user/alloc_room", methods=["POST"])
+def user_alloc_room():
+	user_params = request.json
+	
+	try:
+		date_schdl = col_room_allocs.find({user_params["date"]: {"$exists": True}})
+
+		if not list(date_schdl):
+			new_scdl = {
+				user_params["date"]: {
+					user_params["slot"]:
+					{
+						user_params["room"]:{
+							'available': False,
+							'assignedTo': user_params['username'],
+							'requestID': str(uuid.uuid4())[:8]
+						}
+					}
+				}
+			}
+			col_room_allocs.insert_one(new_scdl)
+
+		else:
+			print(list(date_schdl))
+
+	except Exception as e:
+		print(f"something bad happened: {e}")
+		return jsonify({"message": "Missing params!"}), 400
+
+	return jsonify({"message": "Test works, ez :)"})
+
+@app.route("/user/view_alloc", methods=["POST"])
+def user_view_alloc():
+	return jsonify({"message": "Test works, ez :)"})
